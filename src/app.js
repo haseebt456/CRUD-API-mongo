45c6eaafa1580a2c1bf720c0f26e1860c5119d80
haseebt456 <m.haseebtufail@gmail.com>
2023-12-10 22:37:11 +0500

    add get method

--- src/app.js
+++ src/app.js
@@ -17,7 +17,17 @@ app.post("/Products",async (req,res)=>{
     catch(e){
         res.status(400).send(e);
     }
-})
+});
+
+app.get("/Products",async (req,res)=>{
+    try{
+        const getProducts = await Products.find()
+        res.send(getProducts);
+    }
+    catch(e){
+        res.status(400).send(e);
+    }
+});
 
 app.listen(port,()=>{
     console.log(`Connection is live at pont no. ${port}`)
