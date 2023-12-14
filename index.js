f5cec262738e5445eb633a137a2ec7512c5927a9
haseebt456 <m.haseebtufail@gmail.com>
2023-12-14 14:28:50 +0500

    Add acces from any origin in CORS

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ require('./src/DB/conn')
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 app.use(router);
 
