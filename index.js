d3148ed4a701a8dc40cdb2a7cb6c7f11e56b8de7
haseebt456 <m.haseebtufail@gmail.com>
2023-12-14 15:39:17 +0500

    Add access from any origin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = require("./src/routers/router");
 const cors = require('cors');
+const bodyParser = require('body-parser');
 require('./src/DB/conn')
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,7 +11,8 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(router);
-
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.listen(port,()=>{
     console.log(`Connection is live at port no. ${port}`)
