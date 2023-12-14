cdbcac7f0115ffe70966ad8fdb20c87465202dc0
haseebt456 <m.haseebtufail@gmail.com>
2023-12-14 13:19:14 +0500

    Add CORS

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(router);
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin to access the resource
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+  
 
 app.listen(port,()=>{
     console.log(`Connection is live at port no. ${port}`)
