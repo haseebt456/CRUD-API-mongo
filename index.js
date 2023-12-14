e25988b3785bf593645f5471a8567645a52256f5
haseebt456 <m.haseebtufail@gmail.com>
2023-12-14 14:24:34 +0500

    Add CORS package

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const router = require("./src/routers/router");
+const cors = require('cors');
 require('./src/DB/conn')
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin to access the resource
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
-  
 app.use(express.json());
 app.use(router);
 
