d973ef00acc7eec436152070ac2eb1f4b20b6a15
haseebt456 <m.haseebtufail@gmail.com>
2023-12-14 14:34:24 +0500

    Add access only from local host

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: "*"
+  origin: "http://127.0.0.1:5500"
 }));
 app.use(express.json());
 app.use(router);
--- src/DB/conn.js
+++ src/DB/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://mhaseebtufail:haseeb123@cluster.ocwdiqu.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://mhaseebtufail:haseeb123@cluster.ocwdiqu.mongodb.net/?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{
     console.log("Connection sucessful");
 }).catch((error)=>{
